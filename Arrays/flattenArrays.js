421ba8e8a72374e8f882ac25084cf53dd3a61186
Madhur Gupta <guptamadhur96@gmail.com>
2020-12-15 23:05:00 +0530

    Flatten the Array

---
+++ Arrays/flattenArrays.js
@@ -0,0 +1,11 @@
+const flattenArray = (arr) => {
+  if (arr == null || !Array.isArray(arr)) return [];
+  return arr.reduce(
+    (a, b) => a.concat(Array.isArray(b) ? flattenArray(b) : b),
+    []
+  );
+};
+
+console.log(flattenArray([1, 2, 3])); // [1,2,3]
+console.log(flattenArray([1, 2, 3, [4, 5]])); // [1,2,3,4,5]
+console.log(flattenArray([1, 2, 3, [4, 5, [6]]])); // [1,2,3,4,5,6]
